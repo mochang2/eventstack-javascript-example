f26a61c223558caf3af0a1c4798d9bf88a43e08c
changmolee <changmolee@netmable.com>
2025-10-27 16:21:45 +0900

    feat: js, vue 파싱 에러가 있는 케이스에 따른 TC 추가

---
+++ src/utils/error.js
@@ -0,0 +1,10 @@
+// [TEST] normally not execute in a files with parsing errors
+
+const aa "fdf";
+
+// [TEST] normally not execute when there are any parsing errors
+// [TEST] normally not execute at the time when the parsing errors are fixed
+// [TEST] normally execute after the parsing errors are fixed
+// function zxcv() {
+//     console.log("zxcv called");
+// }
---
+++ src/views/Error.vue
@@ -0,0 +1,18 @@
+<template>
+    <div></div>
+    <!-- [TEST] normally execute in files with parsing errors only inside the template tag -->
+    <!-- < -->
+</template>
+
+<script>
+// [TEST] normally not execute in files with parsing errors inside the script tag
+
+const aa "fdf";
+
+// [TEST] normally not execute when there are any parsing errors
+// [TEST] normally not execute at the time when the parsing errors are fixed
+// [TEST] normally execute after the parsing errors are fixed
+// function zxcv() {
+//     console.log("zxcv called");
+// }
+</script>
